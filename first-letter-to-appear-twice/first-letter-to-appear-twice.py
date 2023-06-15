f9060a716572e90ee19459dc358e376b5ddbf795
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2023-06-15 17:56:45 +0530

    Time: 40 ms (70.34%) | Memory: 16.3 MB (49.18%) - LeetSync

---
+++ first-letter-to-appear-twice/first-letter-to-appear-twice.py
@@ -0,0 +1,12 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        dic= {}
+        for i in s:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+                if dic[i] == 2:
+                    return i
+        return ""
+        
