f3ac759c007c08c395831c94459615ba242bb137
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2023-06-15 18:08:57 +0530

    Time: 61 ms (80.56%) | Memory: 16.2 MB (97.31%) - LeetSync

--- minimize-string-length/minimize-string-length.py
+++ minimize-string-length/minimize-string-length.py
@@ -1,5 +1,3 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
-        stack = []
-        stack = list(s)
-        return len(list(set(stack)))
+        return len(list(set(list(s))))
