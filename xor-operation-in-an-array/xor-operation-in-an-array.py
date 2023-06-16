285ec54b6bc27811aef22a60be2e859501744716
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2023-06-16 19:20:01 +0530

    Time: 52 ms (18.08%) | Memory: 16.2 MB (53.62%) - LeetSync

---
+++ xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        arr = []
+        for i in range(n): arr.append(start + (2*i))
+        res = arr[0]
+        for i in range(1, len(arr)): res^=arr[i]
+        return res
