20cbd006dce74eed11ebf52c7e9e5b82a53e58b3
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2023-06-24 11:57:47 +0530

    Time: 91 ms (29.63%) | Memory: 16.4 MB (89.35%) - LeetSync

---
+++ ransom-note/ransom-note.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        dic = {}
+
+        for i in magazine:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        
+        for i in ransomNote:
+            if i not in dic:
+                return False
+            else:
+                if dic[i] > 0:
+                    dic[i] -= 1
+                else:
+                    return False
+        return True
+            
