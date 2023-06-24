1e81c9640685129955cb76a99d6db8f5c03f291a
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2023-06-24 12:12:32 +0530

    Time: 156 ms (7.77%) | Memory: 16.9 MB (92.84%) - LeetSync

--- minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
+++ minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
@@ -4,13 +4,10 @@ class Solution:
         ops = 0
         for i in range(len(nums)-1):
             if nums[i] >= nums[i+1]:
-                print(nums[i], nums[i+1], ops)
                 ops += nums[i] - nums[i+1] + 1
-                print("After ops", ops)
                 nums[i+1]+=nums[i] - nums[i+1] + 1
         if len(nums) >= 2:
             if nums[-2] >= nums[-1]:
-                print(nums[-2], nums[-1])
                 ops+= nums[-2]-nums[-1]+1
         return ops
 
