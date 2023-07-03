521e6a8ab75e7039b4e0dc115013a4afc240d5f2
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2023-07-03 15:49:23 +0530

    Time: 58 ms (17.65%) | Memory: 16.7 MB (28.16%) - LeetSync

--- buddy-strings/buddy-strings.py
+++ buddy-strings/buddy-strings.py
@@ -1,10 +1,9 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
-
-        if len(s) != len(goal):
+        if len(s) != len(goal):  # Check if the lengths of the strings are different
             return False
 
-        if list(set(s)) != list(set(goal)):
+        if list(set(s)) != list(set(goal)):  # Check if the set of characters in 's' is different from the set of characters in 'goal'
             return False
 
         n = len(s)
@@ -12,11 +11,10 @@ class Solution:
         arr = []
 
         for i in range(0, len(s)):
-            if s[i]!=goal[i]:
+            if s[i] != goal[i]:  # Find indices where the characters differ between 's' and 'goal'
                 arr.append(i)
 
-
-        if len(arr) == 0:
+        if len(arr) == 0:  # If 's' and 'goal' are the same, check if there are duplicate characters in 's'
             array = []
 
             for i in s:
@@ -27,17 +25,16 @@ class Solution:
 
             return False
 
-
         li = list(s)
 
-
+        # Swap the characters at the differing indices in 's'
         li[arr[0]], li[arr[1]] = li[arr[1]], li[arr[0]]
 
-        s= ""
-        for i in li: s+=i
+        s = ""
+        for i in li:
+            s += i
 
-        if s == goal:
+        if s == goal:  # Check if 's' is equal to 'goal' after the swap
             return True
 
         return False
-        
