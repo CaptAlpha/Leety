27556b3fda573c63746526b6a0c1fb79361629e7
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2025-03-21 22:20:04 +0530

    Time: 502 ms (11.59%) | Memory: 19.6 MB (94.65%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        supplies = set(supplies)
+        dic = {recipes[i] : ingredients[i] for i in range(len(ingredients))}
+        to_add, res = [], []
+
+        start = True
+        while to_add or start:
+            start = False
+
+            # Update the supply and take away recipes we alr made
+            for i in to_add:
+                del dic[i]
+                supplies.add(i)
+                res.append(i)
+
+            # See which recipes can be made with current supplies
+            to_add = []
+            for key, val in dic.items():
+                can_make = True
+                for i in val:
+                    if i not in supplies:
+                        can_make = False
+                        break
+                if can_make:
+                    to_add.append(key)
+        return res
