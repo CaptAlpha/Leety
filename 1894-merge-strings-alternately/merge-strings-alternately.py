c6388af64471ff73cf131d485a6007664c80d89c
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2025-04-20 18:19:56 +0530

    Time: 39 ms (50.08%) | Memory: 17.6 MB (74.03%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,15 +1,15 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        ans = ""
-        small = word1 if len(word1) < len(word2) else word2
-        large = word1 if len(word1) > len(word2) else word2
-
-
-        n = len(small)
-
-        for i in range(len(small)):
-            ans+=word1[i]+word2[i]
-        ans+=large[len(small):]
-
-        return ans
-        
+        # take two pointers and slowly and check where the pointers stop
+        i, j = 0, 0
+        s = ""
+        while(i < len(word1) and j < len(word2)):
+            s+=word1[i]+word2[j]
+            i +=1
+            j +=1
+        print(s)
+        if i != len(word1):
+            s+= word1[i:]
+        if j != len(word2):
+            s+=word2[j:]
+        return s
