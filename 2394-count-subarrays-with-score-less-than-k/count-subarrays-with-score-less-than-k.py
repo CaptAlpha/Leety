0e0a4c73de4a26cbfc4620842ea36ce34f9ce2e6
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2025-04-28 20:24:05 +0530

    Time: 102 ms (96.83%) | Memory: 30.4 MB (87.54%) - LeetSync

---
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        count = 0
+        left = 0
+        current_sum = 0
+        
+        for right in range(len(nums)):
+            current_sum += nums[right]
+            while current_sum * (right - left + 1) >= k and left <= right:
+                current_sum -= nums[left]
+                left += 1
+            count += right - left + 1
+        
+        return count
