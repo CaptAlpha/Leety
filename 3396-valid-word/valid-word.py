18eee9397d5db0512657a1013ab1b898f76e1183
Arhit Bose Tagore <77459838+CaptAlpha@users.noreply.github.com>
2025-07-16 01:10:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.81%) - LeetSync

--- 3396-valid-word/valid-word.py
+++ 3396-valid-word/valid-word.py
@@ -3,18 +3,15 @@ class Solution:
         vowel = 0
         consonant = 0
         if len(word) <3 or word.isalnum() is False:
-            print("Yes")
             return False
         vowels = ['a', 'e', 'i', 'o', 'u']
 
         for i in word:
             if i.isalpha():
-                print(i, vowels)
                 if i.lower() in vowels:
                     vowel+=1
                 else:
                     consonant+=1
-        print(vowel, consonant)
         if vowel > 0 and consonant > 0:
             return True
         print("last guy :|")
